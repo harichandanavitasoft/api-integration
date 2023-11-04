61cd1d3a4142253425fddbf07d81c79ad7f0d75a
Harichandana <harichandanavita@gmail.com>
2023-11-04 21:16:03 +0530

    first, viewdata components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewDataComponent } from './view-data/view-data.component';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewDataComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
