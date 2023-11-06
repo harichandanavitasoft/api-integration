f264e96b4de6eaf4b347a5996f71eb43a57f5033
Harichandana <harichandanavita@gmail.com>
2023-11-06 12:29:36 +0530

    print apidata using ngfor

--- src/app/view-data/view-data.component.ts
+++ src/app/view-data/view-data.component.ts
@@ -8,12 +8,15 @@ import { ApiService } from '../api.service';
   styleUrls: ['./view-data.component.css']
 })
 export class ViewDataComponent implements OnInit {
+  apiInfo!: any;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getData().subscribe((res: any) => {
       console.log(res);
+      this.apiInfo = res
+      console.log(this.apiInfo);
     });
   }
 
